48bd95d7d053f9642472dd5f6887a5e6ce050c40
Abhay Rana <capt.n3m0@gmail.com>
2013-10-20 02:31:02 +0530

    Google Basic

--- index.php
+++ index.php
@@ -1,20 +1,43 @@
 <?php
- require 'Toro.php';
- require 'lib/facebook.php';
+require 'Toro.php';
+require 'google-api-php-client/src/Google_Client.php';
+require 'google-api-php-client/src/contrib/Google_PlusService.php';
 
-class HelloHandler {
-    function get() {
-        echo "Hello, world";
-    }
-}
+$client = new Google_Client();
+$client->setApplicationName("Rekishi: Year One");
+$client->setClientId('930206244765.apps.googleusercontent.com');
+$client->setClientSecret('5YHs-QqrqX8kSClcSER12NT0');
+$client->setRedirectUri('http://localhost:8080/login/google/cb');
+$client->setDeveloperKey('AIzaSyAbLfaJIOSWDGGSGX3W3RG4JbOJpbWTyAA');
+$plus = new Google_PlusService($client);
 
 class FacebookHandler {
     function get() {
-        Facebook();
+        render('facebook.php');
     }
 }
 
+class GoogleHandler {
+    function get() {
+    	global $client;
+    	$authUrl = $client->createAuthUrl();
+    	header("Location: ".$authUrl);
+    }
+}
+
+class GoogleCBHandler {
+	function get() {
+		global $client;
+		if (isset($_GET['code'])) {
+			$client->authenticate();
+			$_SESSION['token'] = $client->getAccessToken();
+			print_r($_SESSION);
+		}
+	}
+}
+
 Toro::serve(array(
-    "/" => "HelloHandler",
-    "/facebook" => "FacebookHandler" 
+    "/facebook" => "FacebookHandler",
+    "/login/google" => "GoogleHandler",
+    "/login/google/cb" => "GoogleCBHandler"
 ));

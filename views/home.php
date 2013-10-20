7c49d96ed8210fa08cab4752b0598f2757f769b1
Abhay Rana <capt.n3m0@gmail.com>
2013-10-20 11:36:36 +0530

    Fixes font and footer

--- views/home.php
+++ views/home.php
@@ -4,14 +4,19 @@
 		<script type="text/javascript" src="https://apis.google.com/js/plusone.js">
       		{"parsetags": "explicit"}
     	</script>
+    	<link href='http://fonts.googleapis.com/css?family=Pompiere' rel='stylesheet' type='text/css'>
     	<link href="/css/style.css" type="text/stylesheet" rel="stylesheet">
 		<title>Rekishi - Travel in Time</title>
 	</head>
 	<body>
+		<h1>Rekishi - Travel in Time</h1>
 		<h2>Google + Posts</h2>
 		<!--This div will be filled with g+ posts-->
 		<div id="google">
 		</div>
 		<script src="/js/google.js"></script>
+		<footer>
+			We &lt;3 Open Source.
+		</footer>
 	</body>
 </html>

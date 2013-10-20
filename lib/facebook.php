cf3f9224d3ad3147d4614d891013f908aec785f1
ravikishore1993 <ravikishore1993@gmail.com>
2013-10-20 12:14:46 +0530

    Increases range of fb posts

--- lib/facebook.php
+++ lib/facebook.php
@@ -4,7 +4,7 @@
     $me = $facebook->api('/me');
     date_default_timezone_set('Asia/Calcutta');
     $lastyearSameday = strtotime('-1 year');
-    $lastyearNextday = strtotime('-1 year +1 day');
+    $lastyearNextday = strtotime('-1 year +7 day');
     $params = array(
         'method' => 'fql.query',
         'query' => 'SELECT created_time,message,description,share_count, like_info, type, attachment,permalink,action_links,post_id  FROM stream WHERE source_id='.$me["id"].' and created_time>'.$lastyearSameday.' AND created_time<'.$lastyearNextday.' LIMIT 1000'
